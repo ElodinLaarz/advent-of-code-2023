76ae06431249fb1a24746506166575468444a8d7
Caleb Geiger <caleblgeiger@gmail.com>
2023-12-05 21:04:31 -0500

    Day 4 Star 2. Completed!

--- Day4.rs
+++ Day4.rs
@@ -10,51 +10,64 @@ fn scratch_card_points(winning_numbers: &HashSet<usize>, got_numbers: &[usize]) 
             score += 1;
         }
     }
-    if score > 0 {
-        return 2_usize.pow(score - 1);
-    }
-    assert!(score == 0);
-    return 0_usize;
+    // Star 1 - just count the number of winning numbers with scoring.
+    // if score > 0 {
+    //     return 2_usize.pow(score - 1);
+    // }
+    // assert!(score == 0);
+    // return 0_usize;
+
+    // Star 2 - Scratchcards that win scratch cards.
+    return score as usize;
 }
 
-
 fn main() -> std::io::Result<()> {
-    let mut cards: Vec<Vec<char>> = Vec::new();
+    let mut cards: Vec<String> = Vec::new();
     {
         // Is this Rust-ic?
         let args: Vec<String> = env::args().collect();
         if args.len() != 2 {
             panic!("Usage: {} <input file>", args[0]);
         }
-        let fname: &String = &args[1];
+        let fname: &str = &args[1];
         let mut file: File = File::open(fname)?;
         let mut contents: String = String::new();
         file.read_to_string(&mut contents)?;
         for line in contents.lines() {
-            cards.push(line.chars().collect::<Vec<char>>());
+            cards.push(line.to_string());
         }
     }
-    
+
+    let mut duplicated_scratch_cards: Vec<(String, usize)> = cards
+        .iter()
+        .map(|s| (s.clone(), 1))
+        .collect::<Vec<(String, usize)>>();
+
     let mut total_score: usize = 0;
 
-    for card in cards {
-        let preamble_index = card.iter().position(|&c| c == ':').unwrap();
-        let split_index = card.iter().position(|&c| c == '|').unwrap();
-        let winning_numbers = card[preamble_index+1..split_index]
-            .iter()
-            .collect::<String>()
+    for index in 0..duplicated_scratch_cards.len() {
+        let (card, num_of_cards) = duplicated_scratch_cards[index].clone();
+        let preamble_index = card.find(':').unwrap();
+        let split_index = card.find('|').unwrap();
+        let winning_numbers = card[preamble_index + 1..split_index]
             .split(|c: char| !c.is_digit(10))
-            .filter(|s: &&str| !s.is_empty()) // What? Why &&str?
+            .filter(|s: &&str| !s.is_empty())
             .filter_map(|s: &str| s.parse::<usize>().ok())
             .collect::<HashSet<usize>>();
-        let got_numbers = card[split_index+1..]
-            .iter()
-            .collect::<String>()
+        let got_numbers = card[split_index + 1..]
             .split(|c: char| !c.is_digit(10))
             .filter(|s: &&str| !s.is_empty())
             .filter_map(|s: &str| s.parse::<usize>().ok())
             .collect::<Vec<usize>>();
-        total_score += scratch_card_points(&winning_numbers, &got_numbers);
+        // Star 1 - just count the number of winning numbers.
+        // total_score += scratch_card_points(&winning_numbers, &got_numbers);
+
+        // Star 2 - Scratchcards that win scratch cards.
+        let num_of_winning_cards: usize = scratch_card_points(&winning_numbers, &got_numbers);
+        total_score += num_of_cards;
+        for added_index in 1..=num_of_winning_cards {
+            duplicated_scratch_cards[index+added_index].1 += num_of_cards;
+        }
     }
     println!("Total Score: {}", total_score);
 
